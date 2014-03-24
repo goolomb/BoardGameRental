e81e09f53bde482b6fff6a7bac22f5b896a0b0b1
PatNov <pata.nov@seznam.cz>
2014-03-24 18:16:13 +0100

    BoardGameRental

--- test/boardgamerental/CustomerManagerImplTest.java
+++ test/boardgamerental/CustomerManagerImplTest.java
@@ -9,6 +9,9 @@ package boardgamerental;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -80,13 +83,22 @@ public class CustomerManagerImplTest {
      */
     @Test
     public void testFindAllCustomers() {
-        System.out.println("findAllCustomers");
-        CustomerManagerImpl instance = null;
-        List<Customer> expResult = null;
-        List<Customer> result = instance.findAllCustomers();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertTrue(manager.findAllCustomers().isEmpty());
+
+        Customer c1 = new Customer("I.C. Wiener", "New York City", "+420 666 666 666");
+        Customer c2 = new Customer("Hugh Gerection", "California", "+420 123 456 789");
+
+        manager.createCustomer(c1);
+        manager.createCustomer(c2);
+
+        List<Customer> expected = Arrays.asList(c1,c2);
+        List<Customer> actual = manager.findAllCustomers();
+
+        Collections.sort(actual,idComparator);
+        Collections.sort(expected,idComparator);
+
+        assertEquals(expected, actual);
+        assertDeepEquals(expected, actual);
     }
 
     /**
@@ -129,4 +141,12 @@ public class CustomerManagerImplTest {
         assertEquals(expected.getAddress(), actual.getAddress());
         assertEquals(expected.getPhoneNumber(), actual.getPhoneNumber());
     }
+    
+    private static Comparator<Customer> idComparator = new Comparator<Customer>() {
+
+        @Override
+        public int compare(Customer o1, Customer o2) {
+            return Integer.valueOf(o1.getId()).compareTo(Integer.valueOf(o2.getId()));
+        }
+    };
 }
