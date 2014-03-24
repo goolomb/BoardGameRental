3a7485fb46b359fe8fe53ca10ace1c2026929eba
goolomb <martin.golomb@seznam.cz>
2014-03-24 16:44:22 +0100

    commituju

--- src/boardgamerental/BoardGame.java
+++ src/boardgamerental/BoardGame.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+//chichichi, jde to!!! :D:D:D
 package boardgamerental;
 
 import java.math.BigDecimal;
