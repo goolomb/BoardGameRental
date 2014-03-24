8754f6d8519db17a578d94a02b77087c84b99f0c
PatNov <pata.nov@seznam.cz>
2014-03-24 19:11:55 +0100

    BoardGameRental

--- test/boardgamerental/CustomerManagerImplTest.java
+++ test/boardgamerental/CustomerManagerImplTest.java
@@ -101,38 +101,214 @@ public class CustomerManagerImplTest {
         assertDeepEquals(expected, actual);
     }
 
+    @Test
+    public void addCustomerWithWrongAttributes() {
+
+        try {
+            manager.createCustomer(null);
+            fail();
+        } catch (IllegalArgumentException ex) {
+            //OK
+        }
+
+        Customer customer = new Customer("I.C. Wiener", "New York City", "+420 666 666 666");
+        customer.setId(1);
+        try {
+            manager.createCustomer(customer);
+            fail();
+        } catch (IllegalArgumentException ex) {
+            //OK
+        }
+
+        customer = new Customer(null, "New York City", "+420 666 666 666");
+        try {
+            manager.createCustomer(customer);
+            fail();
+        } catch (IllegalArgumentException ex) {
+            //OK
+        }
+
+        customer = new Customer("I.C. Wiener", null, "+420 666 666 666"); 
+        try {
+            manager.createCustomer(customer);
+            fail();
+        } catch (IllegalArgumentException ex) {
+            //OK
+        }
+
+        customer = new Customer("I.C.Wiener", "New York City", null); 
+        try {
+            manager.createCustomer(customer);
+            fail();
+        } catch (IllegalArgumentException ex) {
+            //OK
+        }
+    }
     /**
      * Test of findCustomerByName method, of class CustomerManagerImpl.
      */
     @Test
     public void testFindCustomerByName() {
-        
-    }
+        assertTrue(manager.findCustomerByName("wie").isEmpty());
 
+        Customer c1 = new Customer("I.C. Wiener", "New York City", "+420 666 666 666");
+        Customer c2 = new Customer("Hugh Wierection", "California", "+420 123 456 789");
+
+        manager.createCustomer(c1);
+        manager.createCustomer(c2);
+
+        List<Customer> expected = Arrays.asList(c1,c2);
+        List<Customer> actual = manager.findCustomerByName("wie");
+
+        Collections.sort(actual,idComparator);
+        Collections.sort(expected,idComparator);
+
+        assertEquals(expected, actual);
+        assertDeepEquals(expected, actual);
+    }
+    
     /**
      * Test of updateCustomer method, of class CustomerManagerImpl.
      */
     @Test
     public void testUpdateCustomer() {
-        System.out.println("updateCustomer");
-        Customer customer = null;
-        CustomerManagerImpl instance = null;
-        instance.updateCustomer(customer);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Customer customer = new Customer("I.C. Wiener", "New York City", "+420 666 666 666");
+        Customer c2 = new Customer("Hugh Gerection", "California", "+420 123 456 789");
+        manager.createCustomer(customer);
+        manager.createCustomer(c2);
+        Integer customerId = customer.getId();
+
+        customer = manager.getCustomerById(customerId);
+        customer.setName("Hulla Hop");
+        manager.updateCustomer(customer);        
+        assertEquals("Hulla Hop", customer.getName());
+        assertEquals("New York City", customer.getAddress());
+        assertEquals("+420 666 666 666", customer.getPhoneNumber());
+
+        customer = manager.getCustomerById(customerId);
+        customer.setAddress("Oakland");
+        manager.updateCustomer(customer);        
+        assertEquals("Hulla Hop", customer.getName());
+        assertEquals("Oakland", customer.getAddress());
+        assertEquals("+420 666 666 666", customer.getPhoneNumber());
+
+        customer = manager.getCustomerById(customerId);
+        customer.setPhoneNumber("+420 987 654 321");
+        manager.updateCustomer(customer);        
+        assertEquals("Hulla Hop", customer.getName());
+        assertEquals("Oakland", customer.getAddress());
+        assertEquals("+420 987 654 321", customer.getPhoneNumber());
+
+        // Check if updates didn't affected other records
+        assertDeepEquals(c2, manager.getCustomerById(c2.getId()));
     }
 
+    @Test
+    public void updateCustomerWithWrongAttributes() {
+
+        Customer customer = new Customer("I.C. Wiener", "New York City", "+420 666 666 666");
+        manager.createCustomer(customer);
+        Integer customerId = customer.getId();
+        
+        try {
+            manager.updateCustomer(null);
+            fail();
+        } catch (IllegalArgumentException ex) {
+            //OK
+        }
+        
+        try {
+            customer = manager.getCustomerById(customerId);
+            customer.setId(null);
+            manager.updateCustomer(customer);        
+            fail();
+        } catch (IllegalArgumentException ex) {
+            //OK
+        }
+
+        try {
+            customer = manager.getCustomerById(customerId);
+            customer.setId(customerId - 1);
+            manager.updateCustomer(customer);        
+            fail();
+        } catch (IllegalArgumentException ex) {
+            //OK
+        }
+
+        try {
+            customer = manager.getCustomerById(customerId);
+            customer.setName(null);
+            manager.updateCustomer(customer);        
+            fail();
+        } catch (IllegalArgumentException ex) {
+            //OK
+        }
+
+        try {
+            customer = manager.getCustomerById(customerId);
+            customer.setAddress(null);
+            manager.updateCustomer(customer);        
+            fail();
+        } catch (IllegalArgumentException ex) {
+            //OK
+        }
+
+        try {
+            customer = manager.getCustomerById(customerId);
+            customer.setPhoneNumber(null);
+            manager.updateCustomer(customer);        
+            fail();
+        } catch (IllegalArgumentException ex) {
+            //OK
+        }
+        }
     /**
      * Test of deleteCustomer method, of class CustomerManagerImpl.
      */
     @Test
     public void testDeleteCustomer() {
-        System.out.println("deleteCustomer");
-        Customer customer = null;
-        CustomerManagerImpl instance = null;
-        instance.deleteCustomer(customer);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Customer c1 = new Customer("I.C. Wiener", "New York City", "+420 666 666 666");
+        Customer c2 = new Customer("Hugh Gerection", "California", "+420 123 456 789");
+        manager.createCustomer(c1);
+        manager.createCustomer(c2);
+        
+        assertNotNull(manager.getCustomerById(c1.getId()));
+        assertNotNull(manager.getCustomerById(c2.getId()));
+
+        manager.deleteCustomer(c1);
+        
+        assertNull(manager.getCustomerById(c1.getId()));
+        assertNotNull(manager.getCustomerById(c2.getId()));
+    }
+    
+    @Test
+    public void deleteCustomerWithWrongAttributes() {
+
+        Customer customer = new Customer("I.C. Wiener", "New York City", "+420 666 666 666");
+        
+        try {
+            manager.deleteCustomer(null);
+            fail();
+        } catch (IllegalArgumentException ex) {
+            //OK
+        }
+
+        try {
+            customer.setId(null);
+            manager.deleteCustomer(customer);
+            fail();
+        } catch (IllegalArgumentException ex) {
+            //OK
+        }
+
+        try {
+            customer.setId(1);
+            manager.deleteCustomer(customer);
+            fail();
+        } catch (IllegalArgumentException ex) {
+            //OK
+        }        
+
     }
 
     private void assertDeepEquals(Customer expected, Customer actual) {
@@ -149,4 +325,12 @@ public class CustomerManagerImplTest {
             return Integer.valueOf(o1.getId()).compareTo(Integer.valueOf(o2.getId()));
         }
     };
+
+    private void assertDeepEquals(List<Customer> expectedList, List<Customer> actualList) {
+        for (int i = 0; i < expectedList.size(); i++) {
+            Customer expected = expectedList.get(i);
+            Customer actual = actualList.get(i);
+            assertDeepEquals(expected, actual);
+        }
+    }
 }
